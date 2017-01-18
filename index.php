6f8feb7fa3750db341660203987819832e53ffcb
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-18 14:47:27 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_top.php'); ?>
+<?php require_once('includes/slider.php'); ?>
+<?php require_once('includes/news.php'); ?>
+<?php require_once('includes/why.php'); ?>
+<?php require_once('includes/gyms.php'); ?>
+<?php require_once('includes/treners.php'); ?>
+<?php require_once('includes/calendar.php'); ?>
+<?php require_once('includes/we_grou_up.php'); ?>
+<?php require_once('includes/zapis.php'); ?>
+<?php require_once('includes/about.php'); ?>
+<?php require_once('includes/logos.php'); ?>
+<?php require_once('includes/footer_bottom.php'); ?>
+<?php require_once('includes/footer.php'); ?>
+
+	
+
