fa0971741f0db82ce5cb51236fc3beaf8eb5f528
engine77 <zaur.ka.00@mail.ru>
2017-01-19 11:41:39 +0300

    19

---
+++ includes/treners.php
@@ -0,0 +1,10 @@
+<section class="section section_padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="df-cap">Наши опытные тренера</h2>
+				<p class="df-toptext">Детей обучают мастера спорта и квалифицированные инструкторы в области восточных единоборств, 				со званиями и наградами, являющиеся победителями и призерами олимпиад и конкурсов.</p>
+			</div>
+		</div>
+	</div>
+</section>		
