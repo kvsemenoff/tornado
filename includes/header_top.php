a1dc658653eb8e33acc7a8753331046aac224bc7
Berzeg07 <berezg86@gmail.com>
2017-01-19 14:53:57 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -9,11 +9,11 @@
 			<div class="col-md-4">
 				<div class="dd-social">
 					<div class="dd-social-table">
-						<div class="dd-icon"><img src="img/vk.png" alt=""></div>
-						<div class="dd-icon"><img src="img/inst.png" alt=""></div>
-						<div class="dd-icon"><img src="img/tube.png" alt=""></div>
-						<div class="dd-icon"><img src="img/face.png" alt=""></div>
-						<div class="dd-icon"><img src="img/ok.png" alt=""></div>
+						<div class="dd-icon"><a href="#"><img src="img/vk.png" alt=""></a></div>
+						<div class="dd-icon"><a href="#"><img src="img/inst.png" alt=""></a></div>
+						<div class="dd-icon"><a href="#"><img src="img/tube.png" alt=""></a></div>
+						<div class="dd-icon"><a href="#"><img src="img/face.png" alt=""></a></div>
+						<div class="dd-icon"><a href="#"><img src="img/ok.png" alt=""></a></div>
 					</div>
 				</div>
 			</div>
@@ -32,7 +32,7 @@
 		<div class="dd-menu-container">
 			<ul>
 				<li class="no-padding"><a class="dd-main-style" href="#">Главная</a></li>
-				<li><a class="dd-main-style" href="#">Обучаем</a>
+				<li id="js-hover"><a class="dd-main-style"  href="#">Обучаем</a>
 					<ul class="dd-sub-menu">
 						<li><a href="#">Каратэ</a></li>
 						<li><a href="#">Ушу</a></li>
@@ -41,8 +41,27 @@
 						<li><a href="#">Аэробика</a></li>
 					</ul>
 				</li>
-				<li><a class="dd-main-style" href="#">Тренеры</a></li>
-				<li><a class="dd-main-style" href="#">Спортсмены</a></li>
+				<li id="js-hover"><a class="dd-main-style" href="#">Тренеры</a>
+					<ul class="dd-sub-menu">
+						<li><a href="#">Каратэ</a></li>
+						<li><a href="#">Ушу</a></li>
+						<li><a href="#">Дзюдо</a></li>
+						<li><a href="#">Кикбоксинг</a></li>
+						<li><a href="#">Аэробика</a></li>
+					</ul>
+				</li>
+				<li id="js-hover"><a class="dd-main-style" href="#">Спортсмены</a>
+					<ul class="dd-sub-menu">
+						<li><a href="#">Лучшие спортсмены 2016</a></li>
+						<li><a href="#">Лучшие спортсмены 2015</a></li>
+						<li><a href="#">Лучшие спортсмены 2014</a></li>
+						<li><a href="#">Лучшие спортсмены 2013</a></li>
+						<li><a href="#">Лучшие спортсмены 2012</a></li>
+						<li><a href="#">Лучшие спортсмены 2011</a></li>
+						<li><a href="#">Лучшие спортсмены 2010</a></li>
+						<li><a href="#">Лучшие спортсмены 2009</a></li>
+					</ul>
+				</li>
 				<li><a class="dd-main-style" href="#">Календарь</a></li>
 				<li><a class="dd-main-style" href="#">Галерея</a></li>
 				<li><a class="dd-main-style dd-last" href="#">Контакты</a></li>
@@ -50,3 +69,9 @@
 		</div>
 	</div>
 </section>
+
+<script>
+	$('.dd-menu-container #js-hover').hover(function(){
+		$(this).find('.dd-sub-menu').stop().slideToggle();
+	});
+</script>
