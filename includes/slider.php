c4b13f68c60d94d81b1b40e5d303da5f86b65793
engine77 <zaur.ka.00@mail.ru>
2017-01-19 17:21:30 +0300

    19

---
+++ includes/slider.php
@@ -0,0 +1,65 @@
+<div class="df-section">
+	<div id="df-js-slider">
+		<div class="item">
+			<div class="df-img-slider">	
+				<img  class="img-responsive" src="img/df-slider.jpg" alt="">
+			</div>	
+			<div class="df-slider df-slider_view">Разница между тем, кто ты есть и тем, кем хочешь быть - это то, что ты делаешь.
+				Совершенствуй себя! Совершенствуй свой дух! Совершенствуй своё тело! <br>
+				Вместе с Центром Каратэ "Торнадо"
+			</div>	
+		</div>
+		<div class="df-section">
+			<div class="df-img-slider">	
+				<img  class="img-responsive" src="img/df-slider.jpg" alt="">
+			</div>	
+			<div class="df-slider df-slider_view">Разница между тем, кто ты есть и тем, кем хочешь быть - это то, что ты делаешь.
+				Совершенствуй себя! Совершенствуй свой дух! Совершенствуй своё тело! <br>
+				Вместе с Центром Каратэ "Торнадо"
+			</div>	
+		</div>
+		<div class="df-section">
+			<div class="df-img-slider">	
+				<img  class="img-responsive" src="img/df-slider.jpg" alt="">
+			</div>	
+			<div class="df-slider df-slider_view">Разница между тем, кто ты есть и тем, кем хочешь быть - это то, что ты делаешь.
+				Совершенствуй себя! Совершенствуй свой дух! Совершенствуй своё тело! <br>
+				Вместе с Центром Каратэ "Торнадо"
+			</div>	
+		</div>
+		<div class="df-section">
+			<div class="df-img-slider">	
+				<img  class="img-responsive" src="img/df-slider.jpg" alt="">
+			</div>	
+			<div class="df-slider df-slider_view">Разница между тем, кто ты есть и тем, кем хочешь быть - это то, что ты делаешь.
+				Совершенствуй себя! Совершенствуй свой дух! Совершенствуй своё тело! <br>
+				Вместе с Центром Каратэ "Торнадо"
+			</div>	
+		</div>
+	</div>
+</div>	
+
+<script>
+	var owl2 = $("#df-js-slider");
+    owl2.owlCarousel({
+
+        loop:true,//Зацикливаем слайдер
+        nav:true, //Навигация включена
+        autoplay:false,//автозапуск
+        smartSpeed:1000,//Время движения
+        margin:0,    
+        navText:['<span class="df-left"></span>','<span class="df-right"></span>'],
+        responsive:{
+            0:{
+              items:1
+          },       
+          1000:{
+              items:1
+          },
+          1248:{
+              items:1
+          }
+      }
+
+  });
+</script>			
